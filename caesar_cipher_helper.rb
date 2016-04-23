cd90c5e0e5036a9a77a260a59ee01ce721ff400e
chen7897499 <476964702@qq.com>
2016-04-23 13:29:21 +0800

    Create caesar_cipher_helper.rb

---
+++ caesar_cipher_helper.rb
@@ -0,0 +1,36 @@
+class CaesarCipher
+  def initialize(shift)
+    #your code here
+    @shift = shift
+  end
+
+  def encode(string)
+    puts string
+    string.chars.map do |a|
+      if a.match(/\p{Alpha}/)
+        if (a.downcase.ord+@shift) >  122
+          ((97+((a.downcase.ord+@shift)-122))-1).chr.upcase
+        else
+          (a.ord+@shift).chr.upcase
+        end
+      else
+        a
+      end
+    end.join
+  end
+  
+  def decode(string)
+    #your code here
+    string.chars.map do |a|
+      if a.match(/\p{Alpha}/)
+        if (a.downcase.ord-@shift) <=  96
+          (122-(96-(a.downcase.ord-@shift))).chr.upcase
+        else
+          (a.ord-@shift).chr.upcase
+        end
+      else
+        a
+      end
+    end.join
+  end
+end
