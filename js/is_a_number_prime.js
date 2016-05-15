87b30b829c4a6df71d7db56a36070330451f3311
chen7897499 <476964702@qq.com>
2016-05-15 23:17:36 +0800

    Update is_a_number_prime.js

--- js/is_a_number_prime.js
+++ js/is_a_number_prime.js
@@ -7,4 +7,8 @@ function isPrime(value) {
     return value > 1;
 }
 
+function isPrime(n) {
+  return !(Array(n + 1).join(1).match(/^1?$|^(11+?)\1+$/));
+}
+
 //https://www.thepolyglotdeveloper.com/2015/04/determine-if-a-number-is-prime-using-javascript/
