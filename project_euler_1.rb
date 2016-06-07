fa009ca2cbf665dda668c36af7010cba1ed7b388
chen7897499 <476964702@qq.com>
2016-06-07 22:20:59 +0800

    Create project_euler_1.rb

---
+++ project_euler_1.rb
@@ -0,0 +1,8 @@
+gets.to_i.times do
+    a = gets.to_i
+    p1 = (a-1)/3
+    p2 = (a-1)/5
+    p3 = (a-1)/15
+    sum = ((3*p1*(p1+1))/2) + ((5*p2*(p2+1))/2) - ((15*p3*(p3+1))/2) 
+    puts sum
+end
